b19eef38f95998a67673b342a6ff125b22ef8c10
devsurimlee <devsurimlee@gmail.com>
2023-08-06 20:17:44 +0900

    init: 프로젝트 설정

---
+++ src/main/java/site/bustago/web/bustagoweb/BustagoWebApplication.java
@@ -0,0 +1,13 @@
+package site.bustago.web.bustagoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BustagoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BustagoWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/bustago/web/bustagoweb/BustagoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package site.bustago.web.bustagoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BustagoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
